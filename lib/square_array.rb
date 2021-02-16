270939d1e68d26442c2e7ef47dda256f8ef69003
Jede Brahma <brahmajede@gmail.com>
2021-02-16 02:17:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,13 @@
 def square_array(numbers)
+  while
   numbers = [1,2,3]
   new_array = []
   numbers.length.times do |index|
   new_array.push(numbers[index] ** 2)
   end
   return new_array
-
-
-square_array(new_numbers)
+  while
+  square_array(new_numbers)
   new_numbers = [9,10,16,25]
   new_numbers_array = []
   new_numbers.length.times do |index|

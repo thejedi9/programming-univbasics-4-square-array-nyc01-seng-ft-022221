f2e1c97f091c1840e7b0d49b6eec77498e73fb61
Jede Brahma <brahmajede@gmail.com>
2021-02-16 02:10:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,17 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  numbers = [1,2,3]
+  new_array = []
+  numbers.length.times do |index|
+  new_array.push(numbers[index] ** 2)
+  end
+  return new_array
+
+
+square_array(new_numbers)
+  new_numbers = [9,10,16,25]
+  new_numbers_array = []
+  new_numbers.length.times do |index|
+  new_numbers_array.push(new_numbers[index] ** 2)
+  end
+  return new_numbers_array
 end
